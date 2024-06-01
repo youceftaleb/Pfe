3ca5d27221096bfb14b551391c29de7743ca47b6
Youcef Taleb <youcefusa2@gmail.com>
2024-06-01 23:25:31 +0100

    i added filter for teachers

--- server/api/controllers/enseignant.js
+++ server/api/controllers/enseignant.js
@@ -22,7 +22,98 @@ exports.updateEnseignant = async (req, res) => {
 
 exports.getProfessors = async (req, res) => {
     try {
-        let teachers = await Enseignant.find();//TODO: add queries + limit teachers whose accounts has not been activated yet
+        const { module, location, disponibilite } = req.body;
+
+        let teachers = []
+        let query_1 = [];
+        let query_2 = [];
+        let query_3 = [];
+        let query1Performed = false;
+        let query2Performed = false;
+        let query3Performed = false;
+        if (module) {
+            query1Performed = true;
+            query_1 = await Enseignant.find({ activated: true, modules: { $in: module } })
+        }
+
+        if (location && Object.keys(location).length !== 0 && location.constructor === Object) {
+            query2Performed = true;
+            let query = Enseignant.find({ activated: true });
+
+            if (location.wilaya) {
+                query = query.where('adresse.wilaya').equals(location.wilaya);
+            }
+
+            if (location.ville) {
+                query = query.where('adresse.ville').equals(location.ville);
+            }
+
+            query_2 = await query.exec();
+        }
+
+        if (disponibilite && Object.keys(disponibilite).length !== 0 && disponibilite.constructor === Object) {
+
+            query3Performed = true;
+            // Build the availability query dynamically
+            let disponibiliteQuery = {};
+
+            if (disponibilite.dayName) {
+                disponibiliteQuery['dayName'] = disponibilite.dayName;
+            }
+
+            if (disponibilite.startTime) {
+                disponibiliteQuery['startTime'] = disponibilite.startTime;
+            }
+            console.log("disp", disponibilite);
+            console.log("disp", disponibiliteQuery);
+            // Add the disponibilite query to the main query if it has conditions
+            if (Object.keys(disponibiliteQuery).length > 0) {
+                // Execute the query
+                query_3 = await Enseignant.find({ activated: true, disponibilite: { $elemMatch: {...disponibiliteQuery} } });
+            }
+
+        }
+
+        function intersectByEmail(arr1, arr2) {
+            return arr1.filter(function (n) {
+                for (var i = 0; i < arr2.length; i++) {
+                    if (n.email === arr2[i].email) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+        }
+
+        if (query1Performed) {
+            if (query2Performed) {
+                teachers = intersectByEmail(query_1, query_2);
+                if (query3Performed) {
+                    teachers = intersectByEmail(teachers, query_3);
+                }
+            } else if (query3Performed) {
+                teachers = intersectByEmail(query_1, query_3)
+            } else {
+                teachers = [...query_1];
+            }
+        } else if (query2Performed) {
+            if (query3Performed) {
+                teachers = intersectByEmail(query_2, query_3);
+            } else {
+                teachers = [...query_2]
+            }
+        } else if (query3Performed) {
+            teachers = [...query_3]
+        } else {
+            teachers = await Enseignant.find({ activated: true })
+        }
+
+        // const mapFromTeachers = new Map(
+        //     teachers.map(c => [c.id, c])
+        // );
+
+        // teachers = [...mapFromTeachers.values()];
+
         teachers = teachers.map(obj => {
             const { password, ...user } = obj._doc;
             return user
